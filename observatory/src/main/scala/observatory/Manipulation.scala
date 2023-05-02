cae5432c70b29015b5457a195a3e59a8bd8d7172
spamegg <spamegg@pm.me>
2023-05-02 12:43:18 +0300

    adding memoization to Manipulation

--- observatory/src/main/scala/observatory/Manipulation.scala
+++ observatory/src/main/scala/observatory/Manipulation.scala
@@ -9,18 +9,28 @@ object Manipulation extends ManipulationInterface:
 
   /**
     * @param temperatures Known temperatures
-    * @return A function that, given a latitude in [-89, 90] and a longitude 
+    * @return A function that, given a latitude in [-89, 90] and a longitude
     *         in [-180, 179], returns the predicted temperature at this location
     */
   def makeGrid(temperatures: Iterable[(Location, Temperature)])
                            : GridLocation => Temperature =               // TODO
-    gLoc => predictTemperature(temperatures, Location(gLoc.lat, gLoc.lon))
+    // gLoc => predictTemperature(temperatures, Location(gLoc.lat, gLoc.lon))
+
+    // memoization solution
+    val memo = collection.mutable.Map[GridLocation, Temperature]()
+    val fun = (gLoc: GridLocation) => memo.get(gLoc) match
+      case None =>
+        val res = predictTemperature(temperatures, Location(gLoc.lat, gLoc.lon))
+        memo += gLoc -> res                                        // memoize it
+        res
+      case Some(value) => value                        // look up memoized value
+    fun
 
   /**
-    * @param temperaturess Sequence of known temperatures over the years 
+    * @param temperaturess Sequence of known temperatures over the years
     *                      (each element of the collection
     *                      is a collection of pairs of location and temperature)
-    * @return A function that, given a latitude and a longitude, 
+    * @return A function that, given a latitude and a longitude,
     *         returns the average temperature at this location
     */
   def average(temperaturess: Iterable[Iterable[(Location, Temperature)]])
@@ -40,4 +50,6 @@ object Manipulation extends ManipulationInterface:
   def deviation(temperatures: Iterable[(Location, Temperature)],
                      normals: GridLocation => Temperature)
                             : GridLocation => Temperature =              // TODO
-    gridLoc => makeGrid(temperatures)(gridLoc) - normals(gridLoc)
+    val fun = makeGrid(temperatures)                         // use memoization!
+    val devFun = (gLoc: GridLocation) => fun(gLoc) - normals(gLoc)
+    devFun
